2efb3545e7771d90ff1b7070a595274f058a4c63
Evan Dolatowski <86629089+GnarlyLasagna@users.noreply.github.com>
2021-12-15 12:21:33 -0600

    Add files via upload

---
+++ 1. Algorithmic Thinking, Peak Finding/findPeakUtil.py
@@ -0,0 +1,29 @@
+
+def findPeakUtil(arr, low, high, n):
+	
+	mid = low + (high - low)/2
+	mid = int(mid)
+	
+	if ((mid == 0 or arr[mid - 1] <= arr[mid]) and
+		(mid == n - 1 or arr[mid + 1] <= arr[mid])):
+		return mid
+		#If middle element is not peak
+		#and its left neighbor is
+		#greater than it, then left half
+		#must have peak . else the right half has peak
+	elif (mid > 0 and arr[mid - 1] > arr[mid]):
+		return findPeakUtil(arr, low, (mid - 1), n)
+
+	else:
+		return findPeakUtil(arr, (mid + 1), high, n)
+
+def findPeak(arr, n):
+
+	return findPeakUtil(arr, 0, n - 1, n)
+
+
+# Driver code
+arr = [1, 3, 20, 4, 1, 0]
+n = len(arr)
+print("Index of a peak point is", findPeak(arr, n))
+
